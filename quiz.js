f064a6d3f7c8333a7a0f664eed97e1280c495348
raunakbanerjee <76702613+raunakbanerjee@users.noreply.github.com>
2021-01-13 20:47:05 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,4 @@
-//add
-const correctAnswers = ['A', 'B', 'B', 'B'];
+const correctAnswers = ['A','B','B','D','C','D','C','D','A','B'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
@@ -7,12 +6,12 @@ form.addEventListener('submit', e => {
   e.preventDefault();
 
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
 
   // check the answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]){
-      score += 25;
+      score += 10;
     }
   });
 
